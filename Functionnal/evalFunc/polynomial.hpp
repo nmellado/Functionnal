2f9855b58638132bb37e661e1c8680d2a70527e0
Nicolas Mellado <nmellado0@gmail.com>
2017-10-05 14:02:06 +0200

    Fix CWisePolynomial implementation + documentation

--- Functionnal/evalFunc/polynomial.hpp
+++ Functionnal/evalFunc/polynomial.hpp
@@ -14,41 +14,43 @@
 
 namespace Functionnal{
 /*!
- * \brief Base class used to evaluate a polynomial function of arbitrary degree
- * in n dimension. Here embedding and embedded spaces share the same dimensions.
- * For multi-dimensionnal vectors, each dimension is treated a scalar value and
- * evaluated with the same polynomial. For instance, when dimension = 3,
- * polynomials with degree 3 are evaluated as:
+ * \brief Base class used to evaluate a polynomial function of arbitrary degree,
+ * on each component of a n-dimensional vector (n*1D evaluation). As a result,
+ * embedding and embedded spaces avec the same number of dimensions: each
+ * dimension is treated a scalar value, but all dimensions are evaluated with
+ * the same polynomial. For instance, when dimension = m, a polynomial with
+ * degree n is evaluated as:
  * \code
  * c =  a0   a1   a2   ....  an
  * X =
- *      1    1    1
- *      x    y    z
- *      x^2  y^2  z^2
- *      ...  ...  ...
- *      x^n  y^n  z^n
+ *      1     1     ...  1
+ *      x0    x1    ...  xm
+ *      x0^2  x1^2  ...  xm^2
+ *      ...   ...   ...  ...
+ *      x0^n  x1^n  ...  xm^n
  *
  * f(x) = c * X;
  * \endcode
  */
-template <typename _Scalar, int _Degree, int _Dim >
+template <typename _Scalar, int _Degree, int _NbElement >
 struct CWisePolynomialEvalFunc{
 
     typedef _Scalar Scalar;
     enum{
-        Degree  = _Degree,
-        Dim     = _Dim,
-        NbCoeff = (_Degree+1) * Dim
+        Degree    = _Degree,
+        Dim       = 1,
+        NbElement = _NbElement,
+        NbCoeff   = (_Degree+1)
     };
     //! Vector type in the parametric domain, defines a position in the parametric domain
-    typedef Eigen::Matrix<Scalar, Dim, 1> EmbeddedVectorType;
+    typedef Eigen::Matrix<Scalar, NbElement, 1> EmbeddedVectorType;
 
     //! Vector type in the embedding domain
-    typedef Eigen::Matrix<Scalar, Dim, 1> EmbeddingVectorType;
+    typedef Eigen::Matrix<Scalar, NbElement, 1> EmbeddingVectorType;
 
     typedef Eigen::Matrix<Scalar, 1, NbCoeff> CoeffType;
 
-    typedef CWisePolynomialEvalFunc<Scalar, Degree-1, Dim> Derivative;
+    typedef CWisePolynomialEvalFunc<Scalar, Degree-1, NbElement> Derivative;
 
     inline
     CWisePolynomialEvalFunc(){}
@@ -62,8 +64,7 @@ struct CWisePolynomialEvalFunc{
     void
     initCoeffs(CType& coeffs){
         coeffs = CoeffType::Zero();
-        if (NbCoeff >= 2)
-            coeffs.template block<1, _Dim>(0, _Dim) = EmbeddingVectorType::Ones();
+        if (NbCoeff >= 2) coeffs( 1 ) = Scalar(1.0);
     }
 
     /*!
@@ -74,7 +75,7 @@ struct CWisePolynomialEvalFunc{
      * \param coeffs Storing \f$coeffs = [a0, a1, ..., a_{Degree}]\f$     *
      *          Initialized as
      * \code
-     * curve.coeffs << 0, 1, 2, 3; // x + 2x^2 + 3x^3
+     * curve.coeffs() << 0, 1, 2, 3; // x + 2x^2 + 3x^3
      * \endcode
      */
     static inline
@@ -89,10 +90,10 @@ struct CWisePolynomialEvalFunc{
         // ...  ...  ...
         // x^n  y^n  z^n
         // where n is the polynom degree
-        Eigen::Matrix<Scalar, NbCoeff, Dim> M;
-        M.row(0) = Eigen::Matrix<Scalar, 1, Dim>::Ones().array();
+        Eigen::Matrix<Scalar, NbCoeff, NbElement> M;
+        M.row(0) = Eigen::Matrix<Scalar, 1, NbElement>::Ones().array();
         for(int i = 1; i != NbCoeff; ++i)
-            M.row(i) = M.row(i-1).array() * x.transpose().array();
+            M.row(i) = x.transpose().array().pow(i);
 
         // eval
         return coeffs * M; //coeffs * M;
@@ -105,14 +106,14 @@ struct CWisePolynomialEvalFunc{
                      int n = NbCoeff-1)
     {
         if(n == 0){
-            stream << "[" << coeffs.col(n).transpose() << "]" << std::flush;
+            stream << coeffs(n) << std::flush;
         }else if (n == 1){
             if ( coeffs(n) != 0)
-                stream << "["  << coeffs.col(n).transpose() << "] x + ";
+                stream << coeffs(n) << " x + ";
             staticPrint(stream, coeffs,n-1);
         }else {
             if ( coeffs(n) != 0)
-                stream << "["  << coeffs.col(n).transpose() << "] x^" << n << " + ";
+                stream << coeffs(n) << " x^" << n << " + ";
             staticPrint(stream, coeffs, n-1);
         }
     }
